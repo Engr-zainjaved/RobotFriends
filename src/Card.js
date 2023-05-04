3c22ec8f6c59c3a8d2d2fbbf750f508867fb0e7e
Engr-zainjaved <2017ch272@student.uet.edu.pk>
2023-05-04 14:38:50 +0500

    second commit

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,15 @@
 import React from 'react';
+
+
  
-const Card = () =>{
+const Card = ({name, email, id}) =>{
+
     return(
-        <div>
-            <img src="https://robohash.org/robot?200x200" alt="robots" />
+        <div className='font-medium text-center bg-green-300 inline-block p-3 m-3 transform transition duration-500 hover:scale-105 rounded-lg shadow-lg'>
+            <img src={`https://robohash.org/${id}?200x200" alt="robots`} />
             <div>
-                <h2>Jane Doe</h2>
-                <p>janedoe@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     )
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Zain Robot',
+        username: 'Zain',
+        email: 'zainrobot@email.com'
+    },
+    {
+        id: 2,
+        name: 'Saad Robot',
+        username: 'Saad',
+        email: 'saadrobot@email.com'
+    },
+    {
+        id: 3,
+        name: 'Ali Robot',
+        username: 'Ali',
+        email: 'alirobot@email.com'
+    },
+    {
+        id: 4,
+        name: 'Zeeshan Robot',
+        username: 'Zeeshan',
+        email: 'zeeshanrobot@email.com'
+    },
+    {
+        id: 5,
+        name: 'Tamoor Robot',
+        username: 'Tamoor',
+        email: 'tamoorrobot@email.com'
+    },
+    {
+        id: 6,
+        name: 'Umar Robot',
+        username: 'Umar',
+        email: 'umarrobot@email.com'
+    },
+    {
+        id: 7,
+        name: 'Talha Robot',
+        username: 'Talha',
+        email: 'talharobot@email.com'
+    },
+    {
+        id: 8,
+        name: 'Asad Robot',
+        username: 'Asad',
+        email: 'asadrobot@email.com'
+    },
+    {
+        id: 9,
+        name: 'Nouman Robot',
+        username: 'Nouman',
+        email: 'noumanrobot@email.com'
+    },
+    {
+        id: 10,
+        name: 'Rehan Robot',
+        username: 'Rehan',
+        email: 'rehanrobot@email.com'
+    }
+]
