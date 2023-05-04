202c7458c1d5ade943ad4b0039c30fc6f2d5af79
Engr-zainjaved <2017ch272@student.uet.edu.pk>
2023-05-04 19:11:34 +0500

    ready gui

--- src/Card.js
+++ src/Card.js
@@ -5,8 +5,8 @@ import React from 'react';
 const Card = ({name, email, id}) =>{
 
     return(
-        <div className='font-medium text-center bg-green-300 inline-block p-3 m-3 transform transition duration-500 hover:scale-105 rounded-lg shadow-lg'>
-            <img src={`https://robohash.org/${id}?200x200" alt="robots`} />
+        <div className='font-medium text-center bg-green-300 inline-block p-3 m-3 transform transition duration-450 hover:scale-105 rounded-lg shadow-lg'>
+            <img alt='robots' src={`https://robohash.org/${id}?200x200" alt="robots`} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
---
+++ src/SearchBox.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchBox = () => {
+    return (
+        <div className="p-2">
+        <input className="p-2 border border-green-400 bg-blue-50" type='search' placeholder='search robots'/>
+        </div>
+    );
+}
+
+export default SearchBox;
